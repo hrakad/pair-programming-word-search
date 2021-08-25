3875760b6ef097b8ebf31471f3dc0a32c1e5f7a3
hrakad <your@email.com>
2021-08-25 02:53:50 +0000

    Pair programme with Neha

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,33 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = (letters, word) => {
+const horizontalJoin = letters.map(ls => ls.join(''))
+const flipt = transpose(letters).map(ls => ls.join(''));
+  for (l in horizontalJoin) {
+    // console.log(flipt[l], word)
+    // console.log(horizontalJoin[l], word);
+    if (horizontalJoin[l] && horizontalJoin[l].includes(word)) {
+      return true;
     }
+    
+    if (flipt[l] && flipt[l].includes(word)) {
+      return true;
+    }
+  }
+  return false;
 }
-
-module.exports = wordSearch
+    
+  
+const transpose = function(matrix) {
+let newMatrix = []
+  for (let i = 0; i < matrix[0].length; i++) {
+    newMatrix[i] = []
+  }
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      newMatrix[j][i] = matrix[i][j];
+    }
+  }
+  return newMatrix;
+}
+  
+  
+module.exports = wordSearch;    
